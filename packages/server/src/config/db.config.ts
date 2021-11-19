3d553ce2fcfe98c84874bc135bef3e7b57b3e810
matt <matt@kaijucode.com>
2021-11-19 11:39:15 -0700

    brought in db config and typeorm

---
+++ packages/server/src/config/db.config.ts
@@ -0,0 +1,30 @@
+import { envConfig } from './env.config';
+const { isProduction, isDevelopment } = envConfig;
+
+const type = 'postgres';
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 5432;
+const username = process.env.DB_USERNAME || 'local';
+const password = process.env.DB_PASSWORD || 'local';
+const database = process.env.DB_NAME || 'usufruct';
+const entities = ['src/**/**.entity{.ts,.js}'];
+const synchronize = isDevelopment;
+
+export const dbConfig: any = isProduction
+  ? {
+      type,
+      host,
+      port,
+      synchronize,
+      entities,
+    }
+  : {
+      type,
+      host,
+      port,
+      username,
+      password,
+      database,
+      entities,
+      synchronize,
+    };
