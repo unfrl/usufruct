16f0092268c86917124a3dec190733084316e521
andrewnoyes <andrew@noyes.io>
2021-11-23 15:36:39 -0700

    display progress if auth store initializing

--- packages/web/src/components/layout/app-bar.tsx
+++ packages/web/src/components/layout/app-bar.tsx
@@ -2,25 +2,67 @@ import {
   AppBar as MuiAppBar,
   Box,
   Button,
+  CircularProgress,
   Link,
   Toolbar,
   Typography,
 } from '@mui/material';
+import { observer } from 'mobx-react';
 import { Link as RouterLink } from 'react-router-dom';
+import { useStores } from '../../hooks';
+import { AuthStatus } from '../../stores';
 
 export interface AppBarProps {
   title: string;
   titleLink?: string;
 }
 
-export const AppBar = (props: AppBarProps) => {
-  const title = props.titleLink ? (
-    <Link component={RouterLink} to={props.titleLink} sx={{ color: 'inherit' }}>
-      {props.title}
-    </Link>
-  ) : (
-    props.title
-  );
+export const AppBar = observer((props: AppBarProps) => {
+  const { auth } = useStores();
+
+  const title = () => {
+    if (props.titleLink) {
+      return (
+        <Link
+          component={RouterLink}
+          to={props.titleLink}
+          sx={{ color: 'inherit' }}
+        >
+          {props.title}
+        </Link>
+      );
+    }
+
+    return props.title;
+  };
+
+  const options = () => {
+    if (
+      auth.status === AuthStatus.Authenticating ||
+      auth.status === AuthStatus.Initializing
+    ) {
+      return <CircularProgress />;
+    }
+
+    // TODO: if authenticated, display user menu
+
+    return (
+      <Box>
+        <Button
+          component={RouterLink}
+          to="/sign-in"
+          variant="text"
+          color="inherit"
+          sx={{ marginRight: 1 }}
+        >
+          Login
+        </Button>
+        <Button component={RouterLink} to="/sign-up" variant="contained">
+          Sign Up
+        </Button>
+      </Box>
+    );
+  };
 
   return (
     <MuiAppBar position="sticky">
@@ -32,23 +74,10 @@ export const AppBar = (props: AppBarProps) => {
         }}
       >
         <Typography variant="h6" component="div">
-          {title}
+          {title()}
         </Typography>
-        <Box>
-          <Button
-            component={RouterLink}
-            to="/sign-in"
-            variant="text"
-            color="inherit"
-            sx={{ marginRight: 1 }}
-          >
-            Login
-          </Button>
-          <Button component={RouterLink} to="/sign-up" variant="contained">
-            Sign Up
-          </Button>
-        </Box>
+        {options()}
       </Toolbar>
     </MuiAppBar>
   );
-};
+});
