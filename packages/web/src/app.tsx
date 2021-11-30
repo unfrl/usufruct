83bbf76ab2d2bcab9ac2e84f15d0aff77395d9d4
andrewnoyes <andrew@noyes.io>
2021-11-30 14:43:51 -0700

    inventory route test

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -6,12 +6,13 @@ import {
 } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import { AppLayout, Toasts } from './components';
 import { getThemedComponents, getThemeOptions } from './config';
 
 const Authentication = React.lazy(() => import('./routes/authentication'));
 const Verification = React.lazy(() => import('./routes/verification'));
+const Inventory = React.lazy(() => import('./routes/inventory'));
 
 export const App = () => {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -25,7 +26,8 @@ export const App = () => {
       <React.Suspense fallback={<Typography>Loading...</Typography>}>
         <Routes>
           <Route path="/" element={<AppLayout />}>
-            <Route index element={<Typography>TODO!</Typography>} />
+            <Route index element={<Link to="inventory">Inventory test</Link>} />
+            <Route path="inventory" element={<Inventory />} />
             <Route
               path="*"
               element={
@@ -35,9 +37,9 @@ export const App = () => {
               }
             />
           </Route>
-          <Route path="/sign-up" element={<Authentication signUp={true} />} />
-          <Route path="/login" element={<Authentication signUp={false} />} />
-          <Route path="/verification" element={<Verification />} />
+          <Route path="sign-up" element={<Authentication signUp={true} />} />
+          <Route path="login" element={<Authentication signUp={false} />} />
+          <Route path="verification" element={<Verification />} />
         </Routes>
       </React.Suspense>
       <Toasts />
---
+++ packages/web/src/routes/inventory.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const Inventory = () => {
+  return (
+    <Box>
+      <Typography>Inventory here!</Typography>
+    </Box>
+  );
+};
+
+export default Inventory;
