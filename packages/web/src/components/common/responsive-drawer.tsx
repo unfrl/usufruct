83cc7c55e944e6d877f18cda2e48f19f16bc8a20
andrewnoyes <andrew@noyes.io>
2021-12-03 13:32:01 -0700

    allow custom options to be provided in drawer header

--- packages/web/src/components/common/responsive-drawer.tsx
+++ packages/web/src/components/common/responsive-drawer.tsx
@@ -9,7 +9,7 @@ import {
   useTheme,
 } from '@mui/material';
 
-const DEFAULT_WIDTH = 600;
+const DEFAULT_WIDTH = 450;
 
 export interface ResponsiveDrawerProps {
   title: string;
@@ -18,13 +18,18 @@ export interface ResponsiveDrawerProps {
   open: boolean;
   onClose: () => void;
   width?: number;
+  /**
+   * By default a close button will be rendered alongside the header title.
+   * Provide `headerOptions` to override the close button with your own controls.
+   */
+  headerOptions?: JSX.Element;
 }
 
 export const ResponsiveDrawer = (props: ResponsiveDrawerProps) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const width = isMobile ? '100vw' : props.width ?? DEFAULT_WIDTH;
-  const { title, anchor, children, open, onClose } = props;
+  const { title, anchor, children, open, onClose, headerOptions } = props;
 
   return (
     <Drawer
@@ -33,19 +38,21 @@ export const ResponsiveDrawer = (props: ResponsiveDrawerProps) => {
       onClose={onClose}
       PaperProps={{ style: { width } }}
     >
-      <AppBar sx={{ position: 'relative' }} color="default">
+      <AppBar position="sticky" color="default">
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography variant="h6" noWrap>
             {title}
           </Typography>
-          <IconButton
-            color="inherit"
-            onClick={onClose}
-            aria-label="close"
-            size="small"
-          >
-            <CloseIcon />
-          </IconButton>
+          {headerOptions ?? (
+            <IconButton
+              color="inherit"
+              onClick={onClose}
+              aria-label="close"
+              size="small"
+            >
+              <CloseIcon />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
       {children}
