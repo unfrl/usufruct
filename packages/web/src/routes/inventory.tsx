c206f505ce4265048fb9b2d85d0b57e3d2505ed4
andrewnoyes <andrew@noyes.io>
2021-12-07 14:50:30 -0700

    testing e2e with loading items

--- packages/web/src/routes/inventory.tsx
+++ packages/web/src/routes/inventory.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { CreateItemDto } from '@unfrl/usufruct-sdk';
 import { observer } from 'mobx-react';
 import React from 'react';
@@ -11,14 +11,30 @@ import {
   ResponsiveDrawer,
 } from '../components';
 import { useStores } from '../hooks';
+import { tryParseRestError } from '../utils';
 
 const Inventory = observer(() => {
-  const { inventory } = useStores();
+  const { inventory, toasts } = useStores();
   const [item, setItem] = React.useState<CreateItemDto>({
     name: '',
     description: '',
   });
   const [open, setOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const init = async () => {
+      try {
+        await inventory.loadAllItems();
+      } catch (error) {
+        toasts.error(tryParseRestError(error));
+      }
+
+      setLoading(false);
+    };
+
+    init();
+  }, []);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -27,27 +43,31 @@ const Inventory = observer(() => {
     <Box>
       <InventoryToolbar onAdd={handleOpen} />
       <Box sx={{ marginBottom: 2 }} />
-      <BasicTable
-        rows={inventory.items}
-        columns={[
-          {
-            key: 'id',
-            title: 'ID',
-          },
-          {
-            key: 'name',
-            title: 'Name',
-          },
-          {
-            key: 'description',
-            title: 'Description',
-          },
-        ]}
-        onRowClick={(row) => {
-          console.log('yooo', row);
-          handleOpen();
-        }}
-      />
+      {loading ? (
+        <Typography>Loading the stuff...</Typography>
+      ) : (
+        <BasicTable
+          rows={inventory.items}
+          columns={[
+            {
+              key: 'id',
+              title: 'ID',
+            },
+            {
+              key: 'name',
+              title: 'Name',
+            },
+            {
+              key: 'description',
+              title: 'Description',
+            },
+          ]}
+          onRowClick={(row) => {
+            console.log('yooo', row);
+            handleOpen();
+          }}
+        />
+      )}
       <ResponsiveDrawer
         keepMounted
         title="New item"
--- packages/web/src/stores/inventory.store.ts
+++ packages/web/src/stores/inventory.store.ts
@@ -1,15 +1,19 @@
 import { Item } from '@unfrl/usufruct-sdk';
+import { makeAutoObservable } from 'mobx';
 import { RootStore } from './root.store';
 
 export class InventoryStore {
   public items: Item[] = [];
 
-  public constructor(private readonly _rootStore: RootStore) {}
+  public constructor(private readonly _rootStore: RootStore) {
+    makeAutoObservable(this);
+  }
 
   public loadAllItems = async () => {
     try {
       const items = await this._rootStore.client.getItems();
-      this.setItems(items);
+      // TODO: should _not_ need to call Object.values -- for some reason it's not respecting the & Item[] type
+      this.setItems(Object.values(items));
     } catch (error) {
       console.error('failed to get items', error);
       throw error;
