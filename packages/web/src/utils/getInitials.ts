4aef912ac7a2c56c3374a682ae5b8ef6c00aafda
andrewnoyes <andrew@noyes.io>
2021-12-10 15:58:54 -0700

    formatting

--- packages/web/src/utils/getInitials.ts
+++ packages/web/src/utils/getInitials.ts
@@ -8,12 +8,11 @@ export const getInitials = (name: string) => {
 
   const split = name.split(' ');
   const first = split[0].substring(0, 1).toUpperCase();
+  const last = split[split.length - 1].substring(0, 1).toUpperCase();
 
   if (split.length === 1) {
     return first;
   }
-
-  const last = split[split.length - 1].substring(0, 1).toUpperCase();
 
   return `${first}${last}`;
 };
