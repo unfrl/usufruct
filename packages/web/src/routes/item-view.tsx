84161acb6656b2acd4ad2f3c022ec50b64803651
andrewnoyes <andrew@noyes.io>
2021-12-20 15:34:06 -0700

    mobile responsive & placeholder image

--- packages/web/src/routes/item-view.tsx
+++ packages/web/src/routes/item-view.tsx
@@ -1,10 +1,24 @@
-import { Chip, Container, Stack, Typography } from '@mui/material';
+import {
+  Chip,
+  Container,
+  Grid,
+  Stack,
+  styled,
+  Typography,
+} from '@mui/material';
 import { Item } from '@unfrl/usufruct-sdk';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Spinner } from '../components';
 import { useStores } from '../hooks';
+import voidImage from '../images/void.svg';
 import { tryParseRestError } from '../utils';
+
+const ItemImage = styled('img')(({ theme }) => ({
+  width: '100%',
+  maxHeight: 200,
+  padding: theme.spacing(2),
+}));
 
 const ItemView = () => {
   const [ready, setReady] = React.useState(false);
@@ -40,22 +54,31 @@ const ItemView = () => {
 
   return (
     <Container maxWidth="md">
-      <Stack spacing={2}>
-        <Stack spacing={1}>
-          <Typography variant="h4">{name}</Typography>
-          <Typography variant="body1">{description}</Typography>
-        </Stack>
-        <Stack direction="row" spacing={1}>
-          {categories.map((category) => (
-            <Chip key={category.id} label={category.name} variant="outlined" />
-          ))}
-        </Stack>
-        <Stack direction="row" spacing={1} flexWrap="wrap">
-          {labels.map((label) => (
-            <Chip key={label.id} label={label.name} size="small" />
-          ))}
-        </Stack>
-      </Stack>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <ItemImage src={voidImage} alt={name} />
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Stack spacing={1}>
+            <Typography variant="h4">{name}</Typography>
+            <Typography variant="body1">{description}</Typography>
+            <Stack direction="row" spacing={1}>
+              {categories.map((category) => (
+                <Chip
+                  key={category.id}
+                  label={category.name}
+                  variant="outlined"
+                />
+              ))}
+            </Stack>
+            <Stack direction="row" spacing={1} flexWrap="wrap">
+              {labels.map((label) => (
+                <Chip key={label.id} label={label.name} size="small" />
+              ))}
+            </Stack>
+          </Stack>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
