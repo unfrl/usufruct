7c570a631270f63d93e4abeca3cf9174c415bea3
andrewnoyes <andrew@noyes.io>
2021-12-27 15:49:29 -0700

    wip layout of home page

--- packages/web/src/routes/home.tsx
+++ packages/web/src/routes/home.tsx
@@ -1,11 +1,30 @@
-import { Typography } from '@mui/material';
+import { Grid, Paper, Stack, styled, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+
+const LibraryItem = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  height: 200,
+}));
+
+const SAMPS = [1, 2, 3, 4, 5, 6];
 
 const Home = () => {
   return (
-    <Link to="inventory">
-      <Typography>Admin inventory UI</Typography>
-    </Link>
+    <Stack>
+      <Link to="inventory">
+        <Typography>Admin inventory UI</Typography>
+      </Link>
+      <Typography variant="h2" fontSize="large" my={2}>
+        Popular libraries (WIP)
+      </Typography>
+      <Grid container spacing={2}>
+        {SAMPS.map((samp) => (
+          <Grid key={samp} item xs={12} sm={6} md={4}>
+            <LibraryItem>{samp}</LibraryItem>
+          </Grid>
+        ))}
+      </Grid>
+    </Stack>
   );
 };
 
