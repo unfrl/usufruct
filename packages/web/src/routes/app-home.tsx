c0ed8510b02ecd4fc77951d8b3fc5ed356db3976
andrewnoyes <andrew@noyes.io>
2021-12-28 15:53:53 -0700

    testing styles on library catalog

--- packages/web/src/routes/app-home.tsx
+++ packages/web/src/routes/app-home.tsx
@@ -1,9 +1,21 @@
-import { Grid, Link, Paper, Stack, styled, Typography } from '@mui/material';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Link,
+  Paper,
+  Stack,
+  styled,
+  Typography,
+} from '@mui/material';
 import { Library } from '@unfrl/usufruct-sdk';
 import { observer } from 'mobx-react';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { useStores } from '../hooks';
+import noData from '../images/no-data.svg';
 import { tryParseRestError } from '../utils';
 
 const LibraryItem = styled(Paper)(({ theme }) => ({
@@ -52,11 +64,37 @@ const AppHome = () => {
               to={`l/${library.slug}`}
               component={RouterLink}
               sx={{ color: 'inherit' }}
-              underline="hover"
             >
-              <LibraryItem>
-                <Typography>{library.name}</Typography>
-              </LibraryItem>
+              <Card sx={{ height: 250 }}>
+                <CardActionArea sx={{ height: '100%' }}>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={noData}
+                    alt={library.name}
+                    sx={{ objectFit: 'contain', padding: 2 }}
+                  />
+                  <CardContent sx={{ paddingTop: 0 }}>
+                    <Typography
+                      noWrap
+                      gutterBottom
+                      title={library.name}
+                      variant="h5"
+                      component="div"
+                    >
+                      {library.name}
+                    </Typography>
+                    <Typography
+                      noWrap
+                      title={library.description}
+                      variant="body2"
+                      color="text.secondary"
+                    >
+                      {library.description || '---'}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
             </Link>
           </Grid>
         ))}
