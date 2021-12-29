d94e20694465a5232e8a83fe70291b5f9900ac64
matt <matt@kaijucode.com>
2021-12-28 17:59:04 -0700

    Now we generat the sdk as sdk-new, which should make it easier to live alongside the old sdk until its replaced

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -9,15 +9,16 @@ rm -rf ../server/out
 # Generate teh swagger-spec.json file from the API
 cd ./packages/server
 OPEN_API_GENERATION_ONLY=true yarn start
+cd ../
 
 # Create the TypeScript client using openapi-generator-cli
 docker run --user 1000:1000 --rm -v ${PWD}:/local \
     openapitools/openapi-generator-cli generate \
-    -i /local/swagger-spec.json \
+    -i /local/server/swagger-spec.json \
     -g typescript-fetch \
-    -o /local/out/ts-fetch \
-    -c /local/swagger-config.json
+    -o /local/sdk-new \
+    -c /local/server/swagger-config.json
 
 # Install dependencies and build the "dist" folder of the client
-cd ./out/ts-fetch
+cd ./sdk-new
 yarn
