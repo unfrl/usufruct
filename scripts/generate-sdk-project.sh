92ed9a87225d2625538a3638a1ea4ec288d1c58e
andrewnoyes <andrew@noyes.io>
2022-01-04 15:54:45 -0700

    part 1 replace sdk

--- scripts/generate-sdk-project.sh
+++ scripts/generate-sdk-project.sh
@@ -4,11 +4,11 @@
 set -e
 
 # Remove existing sdk directory if it exists
-rm -rf packages/sdk-new
+rm -rf packages/sdk
 
 docker run --user 1000:1000 --rm -v ${PWD}/packages:/local \
     openapitools/openapi-generator-cli generate \
     -i /local/server/swagger-spec.json \
     -g typescript-fetch \
-    -o /local/sdk-new \
+    -o /local/sdk \
     -c /local/server/swagger-config.json
