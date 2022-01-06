65f7a44fb15da99ee6e66c375492a15cf9753e21
andrewnoyes <andrew@noyes.io>
2022-01-06 13:59:57 -0700

    attach library member in guard

--- packages/server/src/library/guards/library-member.guard.ts
+++ packages/server/src/library/guards/library-member.guard.ts
@@ -1,8 +1,25 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { UserRequest } from 'src/identity';
+import { LibraryMember } from '../entities';
 import { LibraryMemberService } from '../services';
 
+/**
+ * ID of the custom header used for the library ID. Used to verify a user's membership
+ * and filter records based on the library they belong to.
+ */
 const HEADER_LIBRARY_ID = 'x-usufruct-library-id';
+
+/**
+ * Name of the property that the `library_member` record is set to on the request object.
+ */
+const LIBRARY_MEMBER_PROPERTY_NAME = 'libraryMember';
+
+/**
+ * Extends the `UserRequest` definition with a `libraryMember` field for the user and library.
+ */
+export interface LibraryMemberRequest extends UserRequest {
+  libraryMember: LibraryMember;
+}
 
 /**
  * Pulls the library ID from header and verifies that the user in the request
@@ -17,13 +34,21 @@ export class LibraryMemberGuard implements CanActivate {
 
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest() as UserRequest;
-    if (!request?.user) {
+    if (!request?.user || !request.header(HEADER_LIBRARY_ID)) {
       return false;
     }
 
-    return !!(await this._libraryMemberService.findOne(
+    const libraryMember = await this._libraryMemberService.findOne(
       request.header(HEADER_LIBRARY_ID),
       request.user.id,
-    ));
+    );
+
+    if (!libraryMember) {
+      return false;
+    }
+
+    request[LIBRARY_MEMBER_PROPERTY_NAME] = libraryMember;
+
+    return true;
   }
 }
