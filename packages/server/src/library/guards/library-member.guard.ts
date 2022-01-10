bfc6c2c3d452a65f040cd80f4e6b1f401f3b7f17
andrewnoyes <andrew@noyes.io>
2022-01-10 14:08:57 -0700

    formatting

--- packages/server/src/library/guards/library-member.guard.ts
+++ packages/server/src/library/guards/library-member.guard.ts
@@ -42,7 +42,6 @@ export class LibraryMemberGuard implements CanActivate {
       request.header(HEADER_LIBRARY_ID),
       request.user.id,
     );
-
     if (!libraryMember) {
       return false;
     }
