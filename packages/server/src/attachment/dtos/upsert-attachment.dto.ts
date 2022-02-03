6c95e65ad875e81a5e4397a440f944519df73b7f
andrewnoyes <andrew@noyes.io>
2022-02-03 14:20:02 -0700

    prop validations

--- packages/server/src/attachment/dtos/upsert-attachment.dto.ts
+++ packages/server/src/attachment/dtos/upsert-attachment.dto.ts
@@ -1,7 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPositive } from 'class-validator';
+
 export class UpsertAttachmentDto {
+  @ApiProperty()
+  @IsNotEmpty()
   public name: string;
 
+  @ApiProperty()
+  @IsNotEmpty()
   public contentType: string;
 
+  @ApiProperty()
+  @IsPositive()
   public size: number;
 }
