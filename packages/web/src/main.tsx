73a2bc37b7b6a3cec549f2ac6f9afec58b7b1911
andrewnoyes <andrew@noyes.io>
2022-03-29 15:45:37 -0600

    update to react 18

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+if (container) {
+  ReactDOMClient.createRoot(container).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+}
